b6baf7341fd63af3718dbb15b6430a8d97555473
Brian Ko <brian.kostar@gmail.com>
2016-08-13 11:34:01 -0400

    accessor

--- Classes/accessors.ts
+++ Classes/accessors.ts
@@ -1,11 +1,13 @@
-//default or public, protected, private
+//default or public, protected, private property Modifiers
 //public we know.. accessible anywhere
 
 //private -- cannot be accessed from outside class
 class Animal {
 	protected name: string
 	constructor(theName:string){
-		this.name = theName
+	//constructor parameter property:  //doing this we dont need to declare 'name' at top
+	//constructor(protected name:string){
+			this.name = theName
 	}
 }
 
@@ -41,3 +43,29 @@ var human = new Human('brian');
 
 //animal = cat;		//<--we can set animal to be cat as they share the same private declaration
 //animal = human;		//<--not compatible even when properties are same because it has a different private property reference
+
+
+//Accessors -- get/set methods to fine control getting properties of an obj
+class Employee {
+	name: string;
+}
+let employee = new Employee()
+employee.name = 'brian' 	//this works, but not good because anyone can change the property value.
+
+//we can add passcode to 'set' the property:
+let passcode = 'secret'
+class Employee2 {
+	private _name: string;
+	get name():string {
+		return this._name;
+	}
+	set name(newName: string){
+		if(passcode && passcode =="secret"){
+			this._name = newName;
+		}else{
+			console.log('unauthorized access')
+		}
+	}
+}
+let emp2 = new Employee2()
+emp2.name = 'brian'	//works the same
